cdf38ed56915bb9fc31354fef1ed6dcda5d80f99
RostislavDolbilov <57857499+RostislavDolbilov@users.noreply.github.com>
2020-09-05 10:42:18 +0300

    Update puzzle.js

--- Game Fifteen/script/puzzle.js
+++ Game Fifteen/script/puzzle.js
@@ -1,6 +1,10 @@
 
 <!--Author Rostislav Dolbilov-->
 
+if (gravdept.isIos()) {
+    document.querySelector('html').classList.add('is-ios');
+}
+
 let squares = [];
 let container = document.createElement("div");
 container.classList.add("container");
